89a36c1869dab0d7774f6f97e5969baa516704bd
jood-f <joodfahad21@gmail.com>
2025-06-25 14:23:58 +0300

    update by jood & renad

--- ScatterPlot_StakeholderMap.py
+++ ScatterPlot_StakeholderMap.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 course_files = {
     "BIO390": "field_exp_sp(final)_extracted.json",
     "FNU484": "field_exp sp3_extracted.json",
-    "FNU473": "field_exp sp2_extracted.json"  # أضف باقي الملفات هنا
+  # "FNU473": "field_exp sp2_extracted.json"  # أضف باقي الملفات هنا
 }
 
 # --- الجهات المعنية ---
