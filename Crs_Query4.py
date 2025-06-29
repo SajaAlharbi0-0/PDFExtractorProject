18e6ee703b343922fb890b68f74449ff56d8dc0a
IbtisamGit <ialjadani0004@stu.kau.edu.sa>
2025-06-29 07:59:42 +0300

    Up

--- Crs_Query4.py
+++ Crs_Query4.py
@@ -1,6 +1,4 @@
 import json
-
-# --- ربط كل كورس بملفه ---
 course_files = {
     "STAT110": "crs sp1 (1).json",
     "PHYS110": "crs_sp2 (1).json",
@@ -13,12 +11,8 @@ course_files = {
     "FNU451": "crs sp7.json",
     "BIO444": "crs sp8.json"
 }
-
-# --- إدخال المستخدم ---
 department = input("Enter Department (STAT / PHYS / BIO / FNU / MET): ").strip().upper()
 code = input("Enter Course Code (110 / 241 / 491 / 121 / 471 / 444/ 450 / 451) [Optional]: ").strip()
-
-# --- تحديد المفاتيح المستهدفة ---
 if code:
     course_key = department + code
     if course_key in course_files:
@@ -31,11 +25,8 @@ else:
     if not target_keys:
         print(f"❌ No courses found for department {department}")
         exit()
-
-# --- عرض مصادر التعلم ---
 for key in target_keys:
     json_file = course_files[key]
-
     try:
         with open(json_file, encoding="utf-8") as f:
             data = json.load(f)
